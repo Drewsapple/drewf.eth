fe13bb2e7257d2a972cb211147dda6212b18e5d9
Drew Fisher <awfisher@wpi.edu>
2022-10-12 14:18:44 -0400

    Add Icons for github and twitter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,25 @@
 		id="landing"
 		class="grid grid-rows-3 grid-cols-1 md:grid-cols-3 gap-2 w-screen min-h-screen md:h-screen overscroll-contain"
 	>
-		<div class="md:row-start-2 md:row-span-2 flex items-center">
-			<span id="introduction" class="m-auto p-8">
-				<h2 class="text-gray-900 text-3xl">Hi, I'm</h2>
-				<h1 class="text-violet-600 font-bold text-6xl">Drew Fisher</h1>
-			</span>
+		<div class="md:row-start-2 md:row-span-2 flex-col items-center">
+			<div class="m-auto p-8 w-full flex-col items-center">
+				<div class="mx-auto max-w-fit md:ml-0">
+					<h2 class="text-gray-900 text-2xl sm:text-3xl">Hi, I'm</h2>
+					<h1 class="text-violet-600 font-bold text-4xl sm:text-6xl">Drew Fisher</h1>
+				</div>
+				<div class="my-4 mx-auto md:ml-0 md:mr-auto w-full shadow-lg rounded-md bg-slate-100 flex items-center p-2 space-x-4 max-w-min">
+					<a class="btn btn-ghost btn-square normal-case" href="https://github.com/drewsapple"
+						><img
+							alt="Github"
+							src="/icons/GitHub-Mark-120px-plus.png"
+							class="inline-block h-8 w-8"
+						/></a
+					>
+					<a class="btn btn-ghost btn-square normal-case" href="https://twitter.com/drewsapple111"
+						><img alt="Twitter" src="/icons/Twitter social icons - circle - blue.svg" class="inline-block h-8 w-8" /></a
+					>
+				</div>
+			</div>
 		</div>
 
 		<div id="projects" class="col-auto row-span-3 md:col-start-2 md:col-span-2 flex-col">
