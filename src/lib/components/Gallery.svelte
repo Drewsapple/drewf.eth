4ab5e6ca9918f5efc06a2bca7eda12605e3cea6a
Drew Fisher <awfisher@wpi.edu>
2022-10-18 16:18:15 -0400

    Fix content formatting

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -10,14 +10,14 @@
 		<a href={path} class="card-title"><h2 class="text-2xl font-bold">{title}</h2></a>
 		<p>{summary}</p>
 
-		<div class="flex min-h-fit overflow-x-auto py-2">
+		<div class="flex min-h-fit overflow-x-auto p-2">
 			{#each contents as post}
 				<a class="min-w-fit max-w-xs my-auto mr-4 card shadow-md bg-base-200" href={post.path}>
 					<div class="card-body px-4 py-2 h-48">
 						{#if post.meta}
-							<img src="" class="h-1/2" alt="" />
+							<figure class="h-1/2"><img src="" class="object-cover" alt="" /></figure>
 							<h3 class="text-gray-900 text-xl h-1/4">{post.meta.title ? post.meta.title : ""}</h3>
-							<p class="text-gray-600 h-1/4">{post.meta.subtitle ? post.meta.subtitle : ""}</p>
+							<p class="text-gray-600 h-1/4 text-sm ">{post.meta.subtitle ? post.meta.subtitle : ""}</p>
 						{/if}
 					</div>
 				</a>
--- src/lib/components/Writeup.svelte
+++ src/lib/components/Writeup.svelte
@@ -6,12 +6,15 @@
     <title>{title ? title : "Untitled Post"}</title>
 </svelte:head>
 
-<article class="max-w-2xl mx-auto p-2 prose">
+<article class="max-w-2xl mx-auto p-8 prose">
     {#if title}
     <h1>{title}</h1>
     {/if}
     {#if subtitle}
     <h2>{subtitle}</h2>
+    {/if}
+    {#if title || subtitle}
+    <div class="divider"/>
     {/if}
     <slot />
 </article>
